774721a31526cf3da9b1c5da4082ea7f5a696d2d
Michée lengronne <michee.lengronne@coppint.com>
2022-01-12 11:59:22 +0100

    Update 4_2_worker_node_kubelet.rb
    Signed-off-by: Michée Lengronne <michee.lengronne@coppint.com>

--- controls/4_2_worker_node_kubelet.rb
+++ controls/4_2_worker_node_kubelet.rb
@@ -1,6 +1,6 @@
 title '4.2 Worker Node: Kubelet'
 
-kubelet = attribute('kubelet')
+kubelet = input('kubelet')
 # fallback if kubelet attribute is not defined
 kubelet = kubernetes.kubelet_bin if kubelet.empty?
 
